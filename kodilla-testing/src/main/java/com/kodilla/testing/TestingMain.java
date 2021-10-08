1bd7beb10a40135f6f0bfd72bcc326898d40a1d9
Agnieszka Chalat <kontakt@akramarz.de>
2021-10-08 12:25:20 +0200

    6.3 Creating and testing OddNumbersExterminator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,35 +1,19 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        // Testing SimpleUser class
-        SimpleUser user = new SimpleUser("theForumUser");
 
-        String result = user.getUserName();
-        if(result.equals("theForumUser")){
-            System.out.println(" test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        // Testing Calculator class
-        Calculator calculator = new Calculator();
-        // Testing add method
-        int additionResult = calculator.add(45,120);
-        if(additionResult == 165) {
-            System.out.println("Calculator - Addition: test OK");
-        } else {
-            System.out.println("Calculator - Addition: Error!");
-        }
-        // Testing subtract method
-        int subtractionResult = calculator.subtract(100, 55);
-        if (subtractionResult == 45) {
-            System.out.println("Calculator - Substraction: test OK");
-        } else {
-            System.out.println("Calculator - Substraction: Error!");
-        }
+        List<Integer> myList = new ArrayList<>();
+        OddNumbersExterminator oddExterminator = new OddNumbersExterminator();
+
+        System.out.println(oddExterminator.exterminate(myList));
     }
 }
+
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,58 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test Suite: end");
+    }
+
+    @DisplayName("when the List of numbers is empty " +
+            "then exterminate() method should return empty list of even numbers"
+    )
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        List<Integer> emptyList = new ArrayList<>();
+        OddNumbersExterminator oddExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> evenNumbers = oddExterminator.exterminate(emptyList);
+        //Then
+        Assertions.assertTrue(evenNumbers.isEmpty());
+    }
+    @DisplayName("when the list include even and odd numbers " +
+            "then exterminate() method should return only even numbers"
+    )
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+      //Given
+        List<Integer> normalList = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12);
+        OddNumbersExterminator oddExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer>  evenNumbers = oddExterminator.exterminate(normalList);
+        List<Integer> expectedList = Arrays.asList(2,4,6,8,10,12);
+        //Then
+        Assertions.assertEquals(expectedList, evenNumbers);
+    }
+}
